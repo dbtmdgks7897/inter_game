eb7e07dd7c5a992e6977704a5c2b6afb15d0d603
ysh <dbtmdgks7897@outlook.kr>
2022-08-03 16:21:01 +0900

    h2

---
+++ src/main/java/GameDaoImpl.java
@@ -0,0 +1,183 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class GameDaoImpl implements GameDao{
+    static ResultSet rs = null;
+    static PreparedStatement pstmt = null;
+    static Connection conn = null;
+    public Connection dbConn(){
+        final String driver = "org.mariadb.jdbc.Driver";
+        final String DB_IP = "localhost";
+        final String DB_PORT = "3306";
+        final String DB_NAME = "database";
+        final String DB_URL =
+                "jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
+
+        Connection conn = null;
+//        ResultSet rs = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(DB_URL, "root", "1234");
+            if (conn != null) {
+                System.out.println("DB 접속 성공");
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("드라이버 로드 실패");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("DB 접속 실패");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    @Override
+    public void save(GameDto dto) {
+
+        try {
+            String sql = "INSERT INTO `database`.`game` (`userid`, `userpw`, `name`) VALUES (?, ?, ?);";
+
+            conn = dbConn();
+            pstmt = conn.prepareStatement(sql);
+
+//            System.out.print("아이디 : ");
+//            String userId = sc.next();
+//            System.out.print("패스워드 : ");
+//            String userPw = sc.next();
+//            System.out.print("이름 : ");
+//            String userName = sc.next();
+
+            pstmt.setString(1, dto.getUserId());
+            pstmt.setString(2, dto.getUserPw());
+            pstmt.setString(3, dto.getUserName());
+
+            pstmt.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("error: " + e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public GameDto findIdPw(String userId, String userPw) {
+        Connection conn = dbConn();
+        GameDto udto = null;
+
+        try {
+            String sql = "select * from `game` where userid=? and userpw=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+            pstmt.setString(2, userPw);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                udto = new GameDto();
+                udto.setUserName(rs.getString("name"));
+                udto.setUserBead(rs.getInt("marble"));
+                udto.setUserInd(rs.getInt("id"));
+                udto.setUserId(rs.getString("userid"));
+                udto.setUserPw(rs.getString("userpw"));
+            }
+        } catch (SQLException e) {
+            System.out.println("error: " + e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return udto;
+    }
+
+    @Override
+    public void update(int id, int plusBead) {
+        Connection conn = dbConn();
+
+        try {
+            String sql = "UPDATE `database`.`game` SET `marble`=?, update_at = NOW() WHERE  `id`=?;";
+            pstmt = conn.prepareStatement(sql);
+
+            if (plusBead <= 0) {
+                plusBead += 10;
+            }
+            pstmt.setInt(1, plusBead);
+            pstmt.setInt(2, id);
+
+            // pstmt.executeQuery();
+            int result = pstmt.executeUpdate();
+            if (result == 0) {
+                System.out.println("저장 실패");
+            } else {
+                System.out.println("저장 성공");
+            }
+        } catch (SQLException e) {
+            System.out.println("error: " + e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Connection conn = dbConn();
+        try {
+            String sql = "DELETE FROM `database`.`game` WHERE  `id`=?;";
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, id);
+            // pstmt.executeQuery();
+            int result = pstmt.executeUpdate();
+            if (result == 0) {
+                System.out.println("삭제 실패");
+            } else {
+                System.out.println("삭제 성공");
+            }
+        } catch (SQLException e) {
+            System.out.println("error: " + e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/GameDto.java
@@ -0,0 +1,43 @@
+public class GameDto {
+    private int userInd, userBead;
+    private String userId, userPw, userName;
+
+    public int getUserInd() {
+        return userInd;
+    }
+    public void setUserInd(int userInd) {
+        this.userInd = userInd;
+    }
+
+
+    public int getUserBead() {
+        return userBead;
+    }
+    public void setUserBead(int userBead) {
+        this.userBead = userBead;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+    public String getUserPw() {
+        return userPw;
+    }
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
